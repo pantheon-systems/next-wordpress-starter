61e766d497c65d7a0ffcae54b35f1bfa996a5297
mitchellmarkoff <mmarkoff4m@gmail.com>
2022-08-04 09:42:59 -0700

    config update

--- next.config.js
+++ next.config.js
@@ -6,29 +6,14 @@ require("dotenv").config({
   path: path.resolve(process.cwd(), ".env.development.local"),
 });
 
-let backendUrl, imageDomain;
-if (process.env.WPGRAPHQL_URL === undefined) {
-  backendUrl = `https://${process.env.PANTHEON_CMS_ENDPOINT}`;
-} else {
-  backendUrl = process.env.WPGRAPHQL_URL;
-}
-if (process.env.IMAGE_DOMAIN === undefined) {
-  imageDomain = backendUrl.replace(/^https:\/\//, "");
-  imageDomain = imageDomain.replace("/wp/graphql", "");
-} else {
-  imageDomain = process.env.IMAGE_DOMAIN;
-}
-// remove trailing slash if it exists
-imageDomain = imageDomain.replace(/\/$/, "");
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    backendUrl: backendUrl,
+    backendUrl: process.env.WPGRAPHQL_URL,
   },
   images: {
-    domains: [imageDomain],
+    domains: [process.env.IMAGE_DOMAIN],
   },
 };
 
