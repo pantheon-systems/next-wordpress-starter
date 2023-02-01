facb8554ef62a3a14e2a0749aeb57748171c01d0
pantheon-decoupled-service-user <decoupled-service-user@pantheon.io>
2023-02-01 16:41:36 +0000

    Merge pull request #499 from pantheon-systems/canary

--- next.config.js
+++ next.config.js
@@ -38,21 +38,10 @@ if (process.env.WPGRAPHQL_URL === undefined) {
 // remove trailing slash if it exists
 imageDomain = imageDomain.replace(/\/$/, '');
 
-if (process.env.PANTHEON_ENVIRONMENT_URL) {
-	if (process.env.PANTHEON_ENVIRONMENT_URL.startsWith('live')) {
-		process.env.IS_LIVE_ENVIRONMENT = true;
-	}
-}
-
 const injectedOptions = {};
 if (process.env.PANTHEON_UPLOAD_PATH) {
 	injectedOptions['basePath'] = process.env.PANTHEON_UPLOAD_PATH;
 }
-
-console.log('CONFIG');
-console.log(process.env.PANTHEON_ENVIRONMENT);
-console.log(process.env.PANTHEON_ENVIRONMENT_URL);
-
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
